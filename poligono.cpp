2892c00b27c89bccb0298daf14ee2bc73aaed900
LeandroAugustt <leandro_augusto321@yahoo.com.br>
2016-10-01 21:32:36 -0300

    Update poligono.cpp

--- poligono.cpp
+++ poligono.cpp
@@ -2,6 +2,7 @@
 #include "point.h"
 #include <iostream>
 #include <math.h>
+#include <vector>
 
 using namespace std;
 
@@ -12,12 +13,13 @@ Poligono::Poligono()
 
 void Poligono::inserir_Vertice(float n)
 {
-    Point p;
+    vector Point p;
     float i, j;
 
     for (i=0; i<n; i++){
         for (j=0; j<n; j++){
-            p.set_VectorXY(i,j);
+            p.setX(i);
+            p.setY(j);
         };
     };
 }
